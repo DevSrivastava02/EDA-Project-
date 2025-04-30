fa0ff9eef0c9c9aaa39d460fb8a8f383d55e6334
DevSrivastava02 <Srivastavaharsh427@gmail.com>
2025-05-01 01:58:17 +0530

    Read the Titanic data

---
+++ EDA_project_weak2_end .py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as mp
+import seaborn as se
+
+df=pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
+
+# anaysis the data
+print(df.info())
+print(df.describe())
+#see the first 5 datasets and last 5 dataset
+print(df.head())
+print(df.tail())
+
